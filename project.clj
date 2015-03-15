55a351dca6ee3d71b83112dfa3f073f0896914f6
Alan Thompson <cloojure@gmail.com>
2015-03-15 14:21:20 -0700

    misc

--- project.clj
+++ project.clj
@@ -3,7 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [
+    [org.clojure/clojure        "1.6.0"]
+    [cooljure                   "0.1.22"]
+    [org.clojure/java.jdbc      "0.3.6"]
+    [java-jdbc/dsl              "0.1.1"]
+    [postgresql/postgresql      "8.4-702.jdbc4"]
+  ]
   :main ^:skip-aot pg-demo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/pg_demo/core.clj
+++ src/pg_demo/core.clj
@@ -1,7 +1,44 @@
 (ns pg-demo.core
+  (:require [clojure.java.jdbc      :as jdbc]
+            [java-jdbc.ddl          :as ddl]
+            [java-jdbc.sql          :as sql])
+  (:use cooljure.core)
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def db-spec
+  { :classname "org.postgresql.Driver"
+    :subprotocol "postgresql"
+    :subname "//localhost:5432/alan"
+  ; :user "alan"
+  ; :pass "secret" 
+  })
+
+(defn v0 []
+  (newline)
+  (println "v0")
+
+  (spy :msg "drop"
+    (jdbc/execute! db-spec ["drop table if exists tags cascade"] ))
+
+  (spy :msg "create"
+    (jdbc/db-do-commands db-spec 
+      (ddl/create-table
+        :tags
+        [:id        :serial "primary key"]
+        [:name      :text   "not null"] )))
+
+  (spy :msg "insert"
+    (jdbc/insert! db-spec :tags
+      {:name "Clojure"}
+      {:name "Java"} ))
+
+  (spy :msg "query"
+    (jdbc/query db-spec
+      (sql/select * :tags (sql/where {:name "Clojure"} ))))
+ (newline)
+)
+
+(defn -main []
+ (v0)
+)
+
