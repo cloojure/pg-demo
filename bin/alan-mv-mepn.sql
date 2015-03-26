2c31c874726ad4a0b501494a986c20550b007345
Alan Thompson <cloojure@gmail.com>
2015-03-26 03:09:42 +0000

    misc

---
+++ bin/alan-mv-mepn.sql
@@ -0,0 +1,38 @@
+\timing on
+
+drop   materialized view if exists
+  rmc_m_e_p_n ;
+
+explain analyze
+create materialized view
+  rmc_m_e_p_n
+  as select
+    rm_case_master.assessment_needed,
+    rm_case_master.case_id,
+    rm_case_master.case_num,
+    rm_case_master.country_id,
+    rm_case_master.create_time,
+    rm_case_master.dlp_revision_number,
+
+    rm_case_event.art_code,
+    rm_case_event.code_status,
+    rm_case_event.deleted,
+    rm_case_event.desc_coded,
+    rm_case_event.details,
+    rm_case_event.diagnosis,
+
+    rm_case_product.product_name,
+    rm_case_product.generic_name,
+
+    rm_case_narrative.narrative
+  from
+    rm_case_master,
+    rm_case_event,
+    rm_case_product,
+    rm_case_narrative
+  where
+    rm_case_master.case_id = rm_case_event.case_id and
+    rm_case_master.case_id = rm_case_product.case_id and
+    rm_case_master.case_id = rm_case_narrative.case_id and
+    rm_case_product.product_name ILIKE 'TYLENOL%'  -- 'IBUPROFEN'
+
