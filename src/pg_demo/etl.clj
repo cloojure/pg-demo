7630ad5e2b34a7909fa049d05e8816be8e34e07b
Alan Thompson <cloojure@gmail.com>
2015-04-06 13:53:55 -0700

    misc

--- src/pg_demo/etl.clj
+++ src/pg_demo/etl.clj
@@ -4,14 +4,14 @@
             [pg-demo.ddl            :as ddl]
             [com.climate.claypoole  :as cp]
   )
-  (:use cooljure.core 
+  (:use cooljure.core
         cooljure.misc)
   (:import java.util.TimeZone)
   (:gen-class))
 
 
 (def tx-chunk-size      1000)
-(def pg-threadpool 
+(def pg-threadpool
   (cp/threadpool        4))
 (def src-is-oracle      true)
 (def dest-is-oracle     false)
@@ -20,9 +20,9 @@
 (def src-spec
   { :classname    "oracle.jdbc.OracleDriver"  ; must be in classpath
     :subprotocol  "oracle"
-    :subname      "thin:@//ora-test-1.cksh17mdz5oo.us-west-1.rds.amazonaws.com:1521/ORCL"
-    :user         "argus_mart" 
-    :password     "rxlogix"
+    :subname        "thin:@//10.100.6.231:1521/pvram"
+    :user           "mart_user"
+    :password       "rxlogix"
   } )
 
 (def dest-spec
@@ -32,6 +32,46 @@
     :user           "demo"
     :password       "demo123"
   } )
+
+
+(def column-name-corrections
+  "A map used to rename problematic columns with more acceptable names.  In the future,
+  it would be best if SQL reserved words (or their derivatives) were not used for column
+  names in the first place."
+  { :primary :primary_val
+    :level   :level_val } )
+
+(defn src-set-context [src-conn]
+  (when src-is-oracle
+    (jdbc/execute! src-conn
+      [ "begin
+           pkg_rls.set_context('admin', '1','ARGUS_MART', '#$!AgSeRvIcE@SaFeTy');
+         end; " ] )))
+
+(defn oracle-init-src []
+  (newline)
+  (println "-----------------------------------------------------------------------------")
+  (println "oracle-init-src")
+  ; Stupid Oracle DB will often crash without this set
+  ; Stupid Oracle DB gives bogus timezone offsets
+  (let [timeZone  (TimeZone/getTimeZone "UTC") ]      ; or "America/Los_Angeles"
+    (TimeZone/setDefault timeZone))
+  (newline)
+  (spyx (jdbc/query src-spec "select sysdate as current_day from dual" ))
+
+  ; Show Oracle DB version info
+  (newline)
+  (spyx (jdbc/query src-spec "select BANNER from SYS.V_$VERSION" ))
+
+  ; Show size of main table
+  (newline)
+  (jdbc/with-db-connection [src-conn src-spec]
+    (spy :msg "pkg_rls.set_context()"
+      (src-set-context src-conn))
+    (newline)
+    (spyx (jdbc/query src-conn "select count(*) as result from rm_case_master" ))
+    (newline)
+  ))
 
 (defn drop-create-pg []
   (newline)
@@ -45,56 +85,39 @@
         (print ".")     ; (println " " table-name)
         (flush)
         (jdbc/execute! pg-conn [drop-cmd] )
-        (jdbc/db-do-commands pg-conn 
+        (jdbc/db-do-commands pg-conn
           (apply jdbc/create-table-ddl table-kw (seq table-ddl))))))
   (println))
 
-(def column-name-corrections
-  "A map used to rename problematic columns with more acceptable names.  In the future,
-  it would be best if SQL reserved words (or their derivatives) were not used for column
-  names in the first place."
-  { :primary :primary_val 
-    :level   :level_val } )
-
-
-(defn src-set-context [src-conn]
-  (when src-is-oracle
-    (jdbc/execute! src-conn 
-      [ "begin
-           pkg_rls.set_context('admin', '1','ARGUS_MART', '#$!AgSeRvIcE@SaFeTy');
-         end; " ] )))
-
-(defn oracle-init-src []
-  (newline)
-  (println "-----------------------------------------------------------------------------")
-  (println "oracle-init-src")
-  ; Stupid Oracle DB will often crash without this set
-  ; Stupid Oracle DB gives bogus timezone offsets
-  (let [timeZone  (TimeZone/getTimeZone "UTC") ]      ; or "America/Los_Angeles"
-    (TimeZone/setDefault timeZone))
-  (newline) (spyx (jdbc/query src-spec "select sysdate as current_day from dual" ))
-
-  ; Show Oracle DB version info
-  (newline) (spyx (jdbc/query src-spec "select BANNER from SYS.V_$VERSION" ))
-
-  ; Show size of main table
-  (newline)
-  (jdbc/with-db-connection [src-conn src-spec]
-    (spy :msg "pkg_rls.set_context()" 
-      (src-set-context src-conn))
-    (newline)
-    (spyx (jdbc/query src-conn "select count(*) as result from rm_case_master" )))
-  (newline)
-)
-
-(def src-table-rows 
+(def src-table-rows
   "A map from table-name to table-rows."
   (promise))
 
-(defn -main []
-  (drop-create-pg)
-(System/exit 1)
+(defn count-tables []
+  (newline)
+  (println "-----------------------------------------------------------------------------")
+  (println "Counting rows for all source tables...")
+  (time
+    (jdbc/with-db-connection [src-conn src-spec]
+      (src-set-context src-conn)
+      (deliver src-table-rows
+        (into (sorted-map)
+          (for [table-name (sort (map name (keys ddl/table-ddl))) ]
+            (let [table-rows   (-> (jdbc/query src-conn
+                                        [ (format "select count(*) as result from %s" table-name) ] )
+                                first
+                                :result
+                                long )
+            ]
+              (println (format "%15d  %s" table-rows table-name))
+              {table-name table-rows} )))))))
 
+(defn -main []
+  (oracle-init-src)
+  (count-tables)
+  (drop-create-pg)
+
+  (newline)
   (println "-----------------------------------------------------------------------------")
   (println "complete")
 )
